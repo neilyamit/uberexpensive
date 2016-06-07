f62bfa49929a9791560a6dd9ae6d93031cd3284f
neilyamit <neil@neilyamit.com>
2016-06-07 18:55:55 +0800

    Updated to include slideshow
    Slideshow for special posts

--- js/act.js
+++ js/act.js
@@ -1,34 +1,116 @@
-
 window.onload = function() {
-  "use strict";
+ "use strict";
 
-  var sliderWidth = getSliderWidth();
+ // Get current width of slider
+  var sliderWidth;
+  getSliderWidth();
   function getSliderWidth() {
-      var w = document.querySelector('.slide-wrap').clientWidth;
-      return w;
+      sliderWidth = document.querySelector('.slide-wrap').clientWidth;
+      return sliderWidth;
   }
 
+  // Recalculate on browser resize;
+  window.addEventListener('resize', function() {
+      var sliderWidth = getSliderWidth();
+      sliderOffset(sliderWidth);
+  });
+
+
+  // Offset position of slider items
   var slideItems = document.querySelectorAll('.slide-item');
-  function sliderOffset() {
+  function sliderOffset(width) {
     for (var i = 0; i < slideItems.length; i++) {
-      var offset = i * sliderWidth;
-      console.log(offset);
+      var offset = i * width + 'px';
       slideItems[i].style.left=offset;
     }
   }
-  sliderOffset();
+  sliderOffset(sliderWidth);
 
   var slideLeftBtn = document.getElementById('slide-left');
   var slideRightBtn = document.getElementById('slide-right');
 
-  slideLeftBtn.addEventListener('click', slideLeft);
-  slideRightBtn.addEventListener('click', slideRight);
+  slideLeftBtn.addEventListener('click', function(){
+      slideLeft();
+      setTimeout(function() {
+          disableRightButton();
+          disableLeftButton();
+      }, 1000, false);
+  });
+  slideRightBtn.addEventListener('click', function(){
+      slideRight();
+      setTimeout(function() {
+          disableRightButton();
+          disableLeftButton();
+      }, 1000, false);
+  });
 
   function slideLeft() {
-    console.log("Clicked left");
+    for (var i = 0; i < slideItems.length; i++) {
+        var item = slideItems[i];
+        var offset = parseInt(item.style.left, 10);
+        var offsetUpdate = offset + sliderWidth;
+        moveLeft(item, offset, offsetUpdate);
+    }
   }
 
   function slideRight() {
-    console.log("Clicked right");
+      for (var i = 0; i < slideItems.length; i++) {
+          var item = slideItems[i];
+          var offset = parseInt(item.style.left, 10);
+          var offsetUpdate = offset - sliderWidth;
+          moveRight(item, offset, offsetUpdate);
+      }
+  }
+
+
+  // Animate scroll
+
+  function moveLeft(elem, offset, offsetUpdate) {
+      var left = offset;
+      var goal = offsetUpdate;
+      function frame() {
+          left += 20;
+          elem.style.left=left + 'px';
+          if (left < goal) {
+              window.requestAnimationFrame(frame);
+          }
+      }
+      window.requestAnimationFrame(frame);
+  }
+
+  function moveRight(elem, offset, offsetUpdate) {
+      var left = offset;
+      var goal = offsetUpdate;
+
+      function frame() {
+          left -= 20;
+          elem.style.left=left + 'px';
+          if (left > goal) {
+              window.requestAnimationFrame(frame);
+          }
+      }
+      window.requestAnimationFrame(frame);
+  }
+
+
+  // Disable buttons when no items left to scroll
+  function disableLeftButton() {
+      var pos = parseInt(document.querySelector('.slide-item:first-of-type').style.left, 10);
+      if (pos >= 0) {
+          slideLeftBtn.setAttribute('disabled', 'disabled');
+      }
+      else {
+          slideLeftBtn.removeAttribute('disabled');
+      }
+  }
+
+  function disableRightButton() {
+      var pos = parseInt(document.querySelector('.slide-item:last-of-type').style.left, 10);
+      if (pos === 0) {
+          slideRightBtn.setAttribute('disabled', 'disabled');
+      }
+      else {
+          slideRightBtn.removeAttribute('disabled');
+      }
   }
 };
